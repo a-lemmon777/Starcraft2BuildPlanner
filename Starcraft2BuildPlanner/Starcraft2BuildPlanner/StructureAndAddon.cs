c9bc2071743b6e6c2a7369f292f0cab62ba5befe
Aaron Lemmon <lemmo031@morris.umn.edu>
2016-01-11 17:24:18 -0600

    Added some support for structures connected to addons

---
+++ Starcraft2BuildPlanner/Starcraft2BuildPlanner/StructureAndAddon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Starcraft2BuildPlanner
+{
+    class StructureAndAddon
+    {
+        public BitmapImage Structure { get; set; }
+        public BitmapImage Addon { get; set; }
+    }
+}
